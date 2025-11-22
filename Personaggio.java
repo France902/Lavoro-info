5ad5c7d3f3bb17613936778dcb207b555aa5a0f8
France <frascic231@gmail.com>
2025-11-22 01:12:46 +0100

    Enhance Personaggio class with new methods
    Added methods for moving backward, setting symbol, and getting health and attack values. Implemented a fight method for character combat.

--- Personaggio.java
+++ Personaggio.java
@@ -23,9 +23,16 @@ public class Personaggio {
     public void vaiAvanti(int passo) {
         posX += passo;
     }
+    public void vaiIndietro(int passo) {
+        posX -= passo;
+    }
     
     public String getSimbolo() {
         return simbolo;
+    }
+    
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
     }
     
     public int getPos() {
@@ -36,6 +43,18 @@ public class Personaggio {
         return manaCost;
     }
     
+    public int getVita() {
+        return vita;
+    }
+    
+    public void setVita(int vita) {
+        this.vita = vita;
+    }
+    
+    public int getAttacco() {
+        return attacco;
+    }
+    
     public boolean condMovimento(Torre T) {
         int larghezzaSimbolo = simbolo.length();
         if((posX + larghezzaSimbolo - 5) >= T.getX() && posX <= (T.getX() + T.getLarghezza()-1)) {
@@ -43,5 +62,19 @@ public class Personaggio {
             return true;
         }
         return false;
+    }
+    
+    public String fight(Personaggio Nemico) {
+        int vitaNemico = Nemico.getVita();
+        int attaccoNemico = Nemico.getAttacco();
+        int i;
+        while(this.vita >= 0 && vitaNemico >= 0) {
+            vitaNemico -= this.attacco;
+            this.vita -= attaccoNemico;
+        }
+        Nemico.setVita(vitaNemico);
+        if(this.vita <= 0 && vitaNemico <= 0) return "pareggio";
+        else if(this.vita <= 0) return "sconfitta";
+        else return "vittoria";
     }
 }
